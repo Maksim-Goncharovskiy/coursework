ad35a9b31ad092da4ffe8bb4f88fb8503687c713
Maksim Goncharovskiy <69748746+Maksim-Goncharovskiy@users.noreply.github.com>
2023-05-18 20:16:37 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -24,23 +24,4 @@ int main(int argc, char* argv[]) {
 		input.push_back(Point(x, y));
 	}
 	std::vector<Triangle> result = getTriangulation(input);
-
-	cv::Mat image(2000, 2000, CV_8UC3, cv::Scalar(0, 0, 0));
-	if (!image.data) {
-		std::cout << "Could not open or find the image" << std::endl;
-		exit(EXIT_FAILURE);
-	}
-	for (int i = 0; i < result.size(); i++) {
-
-		cv::Point p1(result[i].A_->x_, result[i].A_->y_), p2(result[i].B_->x_, result[i].B_->y_), p3(result[i].C_->x_, result[i].C_->y_);
-		cv::Scalar colorLine(0, 255, 0);
-		int thicknessLine = 2;
-		cv::line(image, p1, p2, colorLine, thicknessLine);
-		cv::line(image, p2, p3, colorLine, thicknessLine);
-		cv::line(image, p3, p1, colorLine, thicknessLine);
-	}
-	cv::namedWindow("Display window", cv::WINDOW_AUTOSIZE);
-	cv::imshow("Display window", image);
-
-	cv::waitKey(0);
 }
